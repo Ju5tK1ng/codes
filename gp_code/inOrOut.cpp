265820168ade300b9dc9358074c5a6c2c134f679
JustHz <434594598@qq.com>
2024-12-22 03:55:52 +0800

    inOrOut2.0

--- gp_code/inOrOut.cpp
+++ gp_code/inOrOut.cpp
@@ -5,12 +5,14 @@ struct DayData
     string date;
     double ope, high, low, close;
 };
-string INPUT_PATH = "D:\\codes\\gp_code\\dataGold.csv";
-string OUTPUT_PATH = "D:\\gp\\data\\inOrOut.csv";
+string INPUT_PATH = "D:\\gp\\data\\";
+string OUTPUT_PATH = "D:\\gp\\data\\inOrOut";
 string line;
-vector<string> dataList, simulationList;
+vector<string> idList = {"510310", "515180", "512880", "518880", "513090", "159545", "513910", "513260", "159636", "159792", "159985", "513730",
+                         "159696", "513390", "159660", "513850", "513500", };
+vector<string> dataList, simulationList, resultList, resultAllList;
 vector<DayData> dayDataList;
-vector<string> START_DATE = {"2013-07-30"};
+vector<string> START_DATE = {"2024-10-08"};
 // vector<string> START_DATE = {"2015-05-29","2020-07-03","2022-03-09","2023-07-31","2024-10-14"}; // 3.8+
 string float2Str(double f, int decimal = 2)
 {
@@ -28,56 +30,110 @@ string vector2Str(vector<double> vec)
     str = str.substr(0, str.length() - 1);
     return str;
 }
-int main()
+string calcOne(string id, bool start)
 {
-    ifstream dataFile(INPUT_PATH);
+    dataList.clear();
+    ifstream dataFile(INPUT_PATH + id + ".csv");
     while (getline(dataFile, line)) {
         dataList.push_back(line);
     }
     dataFile.close();
-    map<int, int> percent2CntMap;
-    percent2CntMap.clear();
-    bool start = false;
-    double cnt =  0;
-    int interval = 1;
+    int cnt =  0;
     double ope_close = 100;
     double close_ope = 100;
-    double lastClose, startClose;
+    double profit;
+    double lastClose = 0;
     simulationList.clear();
-    simulationList.push_back("date,open,high,low,close,close_ope,ope_close,profit");
+    simulationList.push_back("date,open,close,close_ope,ope_close,profit");
     for (int i = 1; i < dataList.size(); i++)
     {
         DayData dayData;
         string str = dataList[i];
-        dayData.date = dataList[i].substr(0, 10);
-        dayData.ope = stof(dataList[i].substr(11, 5)) * 1000;
-        dayData.high = stof(dataList[i].substr(17, 5)) * 1000;
-        dayData.low = stof(dataList[i].substr(23, 5)) * 1000;
-        dayData.close = stof(dataList[i].substr(29, 5)) * 1000;
+        string num = "";
+        int commaCnt = 0;
+        for (int j = 0; j < str.size(); j++)
+        {
+            if (str[j] == ',')
+            {
+                commaCnt += 1;
+                if (commaCnt == 1)
+                {
+                    dayData.date = num;
+                }
+                else if (commaCnt == 2)
+                {
+                    dayData.ope = stod(num) * 1000;
+                }
+                else if (commaCnt == 3)
+                {
+                    dayData.close = stod(num) * 1000;
+                }
+                else
+                {
+                    break;
+                }
+                num = "";
+            }
+            else
+            {
+                num += str[j];
+            }
+        }
         dayDataList.push_back(dayData);
         if (dayData.date == START_DATE[0])
         {
             start = true;
-            startClose = lastClose;
         }
-        if (start)
+        if (start && lastClose != 0)
         {
+            cnt += 1;
             ope_close = ope_close / dayData.ope * dayData.close;
             close_ope = close_ope / lastClose * dayData.ope;
-            simulationList.push_back(dataList[i] + "," + float2Str(close_ope, 3) + "," + float2Str(ope_close, 3) + "," + float2Str(close_ope * ope_close / 100, 3));
+            profit = close_ope * ope_close / 100;
+            simulationList.push_back(dataList[i] + "," + float2Str(close_ope, 3) + "," + float2Str(ope_close, 3) + "," + float2Str(profit, 3));
         }
         lastClose = dayData.close;
     }
-    cout << "close_ope: " << float2Str(close_ope, 3) << endl;
-    cout << "ope_close: " << float2Str(ope_close, 3) << endl;
-    cout << dayDataList[dayDataList.size() - 1].close << " / " << startClose << endl;
-    cout << "close_close: " << float2Str(dayDataList[dayDataList.size() - 1].close / startClose, 3) << endl;
-    string simualtionPath = OUTPUT_PATH;
-    ofstream simualtionFile(simualtionPath);
+    string simualtionPath = OUTPUT_PATH + id + ".csv";
+    ofstream simulationFile(simualtionPath);
     for (string line : simulationList)
     {
-        simualtionFile << line << endl;
+        simulationFile << line << endl;
     }
-    simualtionFile.close();
+    simulationFile.close();
+    return id + "," + float2Str(close_ope, 3) + "," + float2Str(ope_close, 3) + "," + float2Str(profit, 3) + "," + to_string(cnt) + "," + float2Str((close_ope - 100) / cnt, 3) + "," + float2Str((ope_close - 100) / cnt, 3) + "," + float2Str((profit - 100) / cnt, 3);
+}
+
+int main()
+{
+    resultAllList.clear();
+    resultAllList.push_back("id,close_ope,ope_close,profit,cnt,c2o,o2c,avg");
+    resultList.clear();
+    resultList.push_back("id,close_ope,ope_close,profit,cnt,c2o,o2c,avg");
+    for (string id : idList)
+    {
+        string resultAll = calcOne(id, true);
+        resultAllList.push_back(resultAll);
+
+        string result = calcOne(id, false);
+        cout << result << endl;
+        resultList.push_back(result);
+    }
+
+    string resultAllPath = OUTPUT_PATH + ".csv";
+    ofstream resultAllFile(resultAllPath);
+    for (string line : resultAllList)
+    {
+        resultAllFile << line << endl;
+    }
+    resultAllFile.close();
+
+    string resultPath = OUTPUT_PATH + START_DATE[0] + ".csv";
+    ofstream resultFile(resultPath);
+    for (string line : resultList)
+    {
+        resultFile << line << endl;
+    }
+    resultFile.close();
     return 0;
 }
--- gp_code/parseAkshare.py
+++ gp_code/parseAkshare.py
@@ -1,7 +1,8 @@
 import akshare as ak
 
-fund_etf_hist_em_df = ak.fund_etf_hist_em(symbol="515180", period="daily", start_date="20130101", adjust="")
-print(fund_etf_hist_em_df)
-
-fund_etf_hist_em_df.to_csv("D:\\gp\\data\\515180data.csv", index=False)
-
+ids = {"510310", "515180", "512880", "518880", "513090", "159545", "513910", "513260", "159636", "159792", "159985", "513730",
+       "159696", "513390", "159660", "513850", "513500", }
+for id in ids:
+    fund_etf_hist_em_df = ak.fund_etf_hist_em(symbol=id, period="daily", start_date="20150430", adjust="qfq")
+    print(fund_etf_hist_em_df)
+    fund_etf_hist_em_df.to_csv("D:\\gp\\data\\" + id + ".csv", index=False)
